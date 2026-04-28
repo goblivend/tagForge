c9adfbd1a76d39750ed62ed2f533465ca66544b9
Ivan Imbert <ivan.imbert@epita.fr>
2026-04-28 19:16:06 +0200

    feat: github CI

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; import { AppLayout } from "./components/layout/AppLayout"; import Library from "./pages/Library"; import Playlists from "./pages/Playlists"; import Settings from "./pages/Settings"; function App() { return ( <Router> <Routes> <Route path="/" element={<AppLayout />}> <Route index element={<Library />} /> <Route path="playlists" element={<Playlists />} /> <Route path="settings" element={<Settings />} /> </Route> </Routes> </Router> ); } export default App;
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AppLayout } from "./components/layout/AppLayout";
+import Library from "./pages/Library";
+import Playlists from "./pages/Playlists";
+import Settings from "./pages/Settings";
+function App() {
+    return (
+        <Router basename="/tagForge/">
+            <Routes>
+                <Route path="/" element={<AppLayout />}>
+                    <Route index element={<Library />} />
+                    <Route path="playlists" element={<Playlists />} />
+                    <Route path="settings" element={<Settings />} />
+                </Route>
+            </Routes>
+        </Router>
+    );
+}
+export default App;
